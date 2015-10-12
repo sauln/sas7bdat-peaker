45b5927ac46a8d9c19c6d8986de3bde8c6fcea09
Nathaniel Saul <nat@saulgill.com>
2015-10-11 17:57:07 -0700

    adding test files

--- prototype/test_saspy.py
+++ prototype/test_saspy.py
@@ -3,15 +3,49 @@ import sys
 
 print sys.argv
 
-from sas7bdat import SAS7BDAT
-
+from sas7bdat import SAS7BDAT, SASHeader
 
 f = '../data/beer.sas7bdat'
 
 #sys.argv[1]
 S = SAS7BDAT(f)
-for row in S:
-    print row
+
+l = S.readlines()
+for i in xrange(10):
+    print next(l)
+
+print S.header
+
+
+
+
+
+
+
+#h = f.header.read_page_header()
+
+
+#print "Type of S: %s" %type(S)
+#print "S: %s" %S
+
+
+#print "var(s): %s" %vars(S)
+
+#h = S.header
+#print "type h: %s" %type(h)
+#print "\n\ns.header: %s" %S.header
+
+#print "\n\nREADLINES:\n"
+#print S.readlines()
+
+
+
+
+
+
+   
+#for row in S:
+#    print row
 
 
 #sys.exit(1)
---
+++ prototype/wrap_up.py
@@ -0,0 +1,9 @@
+#need to develop a way - with a smooth build process - to wrap
+#up the entire program into an executable that doesn't need python
+#to run.
+
+#I think I can use py2exe to this
+
+
+
+
